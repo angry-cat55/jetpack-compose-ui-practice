8a1ed73751465ce1a3879cdefeef166f710d03fb
YuJihwan <applewlghks321@gmail.com>
2026-01-12 02:34:27 +0900

    State Hoisting, 콜백 함수

--- app/src/main/java/com/example/composeuitutorial/MainActivity.kt
+++ app/src/main/java/com/example/composeuitutorial/MainActivity.kt
@@ -32,9 +32,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -47,27 +49,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ImageCard()
+            var isFavorite by rememberSaveable { // 상태를 컴포저블에서 위로 올려서 재사용 가능하도록 구성.
+                mutableStateOf(false)
+            }
+            ImageCard(
+                modifier = Modifier // 재사용을 위해 modifier을 위에서 전달
+                    .fillMaxWidth(0.5f)
+                    .padding (16.dp),
+                isFavorite = isFavorite
+            ) /*콜백 함수*/ { favorite ->
+                isFavorite = favorite
+            }
         }
     }
 }
 
 @Composable
-fun ImageCard() {
-    // 컴포즈에서는 특정 데이터의 '상태값'이 변경되면 재구성을 하게 되어 메소드가 재호출됨.
-    val isFavorite = rememberSaveable { // remember 키워드가 붙은 값은, 전 호출 직전의 값을 기억한 후 기본값 설정없이 적용됨.
-        // val 키워드를 쓰는 이유는, remember가 반환한 객체는 변화하지 않고, 멤버(value)의 값만 변경되기 때문에.
-        // 등호(=) 대신 by를 쓰면, .value 형태 대신 객체 이름으로 값을 사용 가능. 단, ~.runtime.getValue, setValue 임포트 필요 (자동 임포트 안 됨.)
-        // remember -> rememberSaveable을 사용해야, 화면을 회전해도 전 데이터를 기억한다.
-        mutableStateOf(false) // 컴포즈가 데이터 값 변화를 인지하고 재구성을 할 수 있도록 하는 메소드
-        // 일반 변수(var a)는 변경이 돼도 재구성이 안 됨.
-    }
-
+fun ImageCard(
+    modifier: Modifier = Modifier, // 외부 수정자 사용(없을 경우 기본값으로 Modifier 적용)
+    isFavorite: Boolean,
+    onTabFavorite: (Boolean) -> Unit
+) {
     Card(
-        modifier = Modifier
-//            .background(color = Color.Blue)
-            .fillMaxWidth(0.5f)
-            .padding (16.dp),
+        modifier = modifier,
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 5.dp
@@ -77,7 +81,6 @@ fun ImageCard() {
             modifier = Modifier
                 .height(200.dp)
         ) {
-            // 이미지 파일이 있을 경우 Image 클래스 중 painter를 사용.
             Image(
                 painter = painterResource(id = R.drawable.img),
                 contentDescription = "비긴어게인 포스터",
@@ -88,11 +91,11 @@ fun ImageCard() {
                 contentAlignment = Alignment.TopEnd,
             ) {
                 IconButton(onClick = {
-                    isFavorite.value = !isFavorite.value
+//                  isFavorite = !isFavorite // 위에 있는 상태는 컴포저블에서 변경 불가능
+                    onTabFavorite(!isFavorite)
                 }) {
-                    // 이미지 파일이 없을 경우 제공되는 백터 이미지를 쓸 수 있는 imageVector 사용
                     Icon(
-                        imageVector = if (isFavorite.value) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
                         contentDescription = "favofite",
                         tint = Color.White
                     )
