252c4e234cd54e5b4fbf0267123f342c02f9595d
YuJihwan <applewlghks321@gmail.com>
2026-01-13 18:53:50 +0900

    Scaffold, TextField, Bitton, 구조분해, SnackBar, 코루틴 스코프

--- app/src/main/java/com/example/composeuitutorial/MainActivity.kt
+++ app/src/main/java/com/example/composeuitutorial/MainActivity.kt
@@ -24,47 +24,76 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeuitutorial.ui.theme.ComposeUiTutorialTheme
+import kotlinx.coroutines.launch
 
+
+/*Todo*/
+// 1. 다 하고 커밋하고 강의 코드대로 친 후 버전 차이 물어보기
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn(
-                modifier = Modifier
-                    .background(color = Color.Green)
-                    .fillMaxWidth(),
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                item {
-                    Text("Header")
+            val (text, setValue) = remember { // mutableStateOf를 구조 분해
+                // text => value값, setValue => 전달받은 String 값으로 value값 변경
+                mutableStateOf("")
+            }
+            // SnackBar 혹은 FloatingAction 버튼 등은 Scaffold를 사용해야 함.
+            val snackbarHostState = remember {
+                SnackbarHostState()
+            }
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
+            Scaffold(
+                snackbarHost = {
+                    SnackbarHost(snackbarHostState)
                 }
-                // LazyColumn에서는 반복문으로 컴포저블 객체 생성 불가능
-                items(50) {index ->
-                    Text("글씨 $index")
-                }
-                item {
-                    Text("Footer")
+            ) {paddingValues ->
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = text,
+                        onValueChange = setValue // TextField의 값이 변경되면 setValue라는 함수 호출
+                    )
+                    Button(onClick = {
+                        keyboardController?.hide() // null이 아닐 때만 호출하는 '안전 호출' 키워드
+                        scope.launch {
+                            snackbarHostState.showSnackbar("Hello $text")
+                        }
+                    }) {
+                        Text("Click!")
+                    }
                 }
             }
         }
